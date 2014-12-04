b06d61c30a38c898b2c80c90b7ad36d91743794a
amigobv <daniell.rotaru@gmail.col>
2014-12-04 11:03:51 +0100

    sql Done

--- src/Domain/createDB.sql
+++
@@ -1,9 +0,0 @@
-""CREATE TABLE if not exists lecture (name varchar(100) primary key);
-CREATE TABLE if not exists document
-(
-  docName varchar(100) primary key,
-  path varchar(200),
-  hash varchar(200),
-  lecture_Name varchar(100),
-  constraint lecture_Name foreign key (lecture_Name) references lecture(name)
-);""
